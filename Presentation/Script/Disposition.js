14da9e06861a7d6d1679562f3556cc33d40b9a9c
Renedeo <62702704+Renedeo@users.noreply.github.com>
2023-11-15 04:38:10 +0100

    Fix : Modification du DOM
    Pour pouvoir avoir l'efft dropdown il fallait un clique sur un element.
    L'erreur est de faire un clic sur la div parente à cette élément.
    Cela a impliquer de faire des modificaton dans style.css

--- Presentation/Script/Disposition.js
+++ Presentation/Script/Disposition.js
@@ -28,31 +28,44 @@ div_Banques = document.createElement("div");
 div_ComptesClient = document.createElement("div");
 div_Transactions = document.createElement("div");
 
+// Ajout des entete des sections
+Banques = document.createElement("div");
+Clients = document.createElement("div");
+ComptesClient = document.createElement("div");
+Transactions = document.createElement("div");
+
 // Ajout de texte dans les différents éléments initiaux
 h1_gestion_banque.appendChild(document.createTextNode("Gestion Banque"));
-div_Banques.appendChild(document.createTextNode("Banques"));
-div_Clients.appendChild(document.createTextNode("Clients"));
-div_ComptesClient.appendChild(document.createTextNode("All accounts"));
-div_Transactions.appendChild(document.createTextNode("All Transactions"));
+Banques.appendChild(document.createTextNode("Banques"));
+Clients.appendChild(document.createTextNode("Clients"));
+ComptesClient.appendChild(document.createTextNode("All accounts"));
+Transactions.appendChild(document.createTextNode("All Transactions"));
 
 // Ajout des flèches
-div_Clients.appendChild(arrow);
-div_Banques.appendChild(arrow.cloneNode(true));
-div_ComptesClient.appendChild(arrow.cloneNode(true));
-div_Transactions.appendChild(arrow.cloneNode(true));
+Clients.appendChild(arrow);
+Banques.appendChild(arrow.cloneNode(true));
+ComptesClient.appendChild(arrow.cloneNode(true));
+Transactions.appendChild(arrow.cloneNode(true));
+
+// Ajout des différentes sections
+div_Clients.appendChild(Clients);
+div_Banques.appendChild(Banques)
+div_ComptesClient.appendChild(ComptesClient)
+div_Transactions.appendChild(Transactions)
 
 // Définition des identifiants
 div_selection_table.id = "div_selection";
-div_Clients.id = "clients";
-div_Banques.id = "banque";
-div_ComptesClient.id = "compteclient";
-div_Transactions.id = "transaction";
+div_Clients.id = "div-clients";
+div_Banques.id = "div-banque";
+div_ComptesClient.id = "div-compteclient";
+div_Transactions.id = "div-transaction";
 
-div_selection_table.appendChild(h1_gestion_banque);
-div_selection_table.appendChild(div_Clients);
-div_selection_table.appendChild(div_Banques);
-div_selection_table.appendChild(div_ComptesClient);
-div_selection_table.appendChild(div_Transactions);
+Clients.id = "clients";
+Banques.id = "banque";
+ComptesClient.id = "compteclient";
+Transactions.id = "transaction";
+
+
 
 if (body.firstChild) {
     body.insertBefore(div_selection_table, body.firstChild);
@@ -60,22 +73,23 @@ if (body.firstChild) {
 
 let arrows = document.querySelectorAll(".arrow-down");
 let config = {
-    clients: {
-        "background-color": "green",
+    "div-clients": {
+        "background-color": "white",
     },
-    banque: {
+    "div-banque": {
         "background-color": "blue",
     },
-    compteclient: {
+    "div-compteclient": {
         "background-color": "yellow",
     },
-    transaction: {
+    "div-transaction": {
         "background-color": "cyan",
     },
-    general: {
+    "general": {
         backgroundColorDefault: "white",
         div_sectionDefault: "gray",
         div_sectionDefaultClassName: "sous_group_div_selection",
+        sectionDefaultClassName: "group_div_selection",
     },
 };
 
@@ -85,20 +99,28 @@ div_Banques.className = config["general"]["div_sectionDefaultClassName"];
 div_ComptesClient.className = config["general"]["div_sectionDefaultClassName"];
 div_Transactions.className = config["general"]["div_sectionDefaultClassName"];
 
-// Initialisation de la couleur de fond
-div_selection_table.style.backgroundColor =
-    config["general"]["backgroundColorDefault"];
+Clients.className = config["general"]["sectionDefaultClassName"];
+Banques.className = config["general"]["sectionDefaultClassName"];
+ComptesClient.className = config["general"]["sectionDefaultClassName"];
+Transactions.className = config["general"]["sectionDefaultClassName"];
+
+// // Initialisation de la couleur de fond
+div_selection_table.style.backgroundColor = config["general"]["backgroundColorDefault"];
 div_Clients.style.backgroundColor = config["general"]["div_sectionDefault"];
 div_Banques.style.backgroundColor = config["general"]["div_sectionDefault"];
-div_ComptesClient.style.backgroundColor =
-    config["general"]["div_sectionDefault"];
-div_Transactions.style.backgroundColor =
-    config["general"]["div_sectionDefault"];
+div_ComptesClient.style.backgroundColor = config["general"]["div_sectionDefault"];
+div_Transactions.style.backgroundColor = config["general"]["div_sectionDefault"];
 
-// Liste des différentes sections de la gestion des banques
+div_selection_table.appendChild(h1_gestion_banque);
+div_selection_table.appendChild(div_Clients);
+div_selection_table.appendChild(div_Banques);
+div_selection_table.appendChild(div_ComptesClient);
+div_selection_table.appendChild(div_Transactions);
+
+// // Liste des différentes sections de la gestion des banques
 div_section = Array.from(
     document.getElementsByClassName(
-        config["general"]["div_sectionDefaultClassName"]
+        config["general"]["sectionDefaultClassName"]
     )
 );
 
@@ -112,18 +134,18 @@ function hide_section(section, id) {
     if (!input_checkbox.checked) {
         div_section = Array.from(
             document.getElementsByClassName(
-                config["general"]["div_sectionDefaultClassName"]
+                config["general"]["sectionDefaultClassName"]
             )
         );
         div_section.forEach(element => {
             if (element.id != id) {
-                element.style.backgroundColor =
+                element.parentNode.style.backgroundColor =
                     config["general"]["div_sectionDefault"];
                 element.style.fontWeight = 'normal';
 
                 element.firstChild.nextSibling.style.transform = "rotate(180deg)";
 
-                table_container = element.querySelector(".table-container");
+                table_container = element.parentNode.querySelector(".table-container");
                 table_container.style.display = "none"
             }
 
@@ -137,25 +159,28 @@ function hide_section(section, id) {
 div_section.forEach((element) => {
     element.addEventListener("click", function (event) {
         if (
-            element.style.backgroundColor == config["general"]["div_sectionDefault"]
+            element.parentNode.style.backgroundColor == config["general"]["div_sectionDefault"]
         ) {
+            
             hide_section(div_section, element.id)
-            element.style.backgroundColor = config[element.id]["background-color"];
+            element.style.borderBottom  = `2px solid aquamarine`;
+            element.parentNode.style.backgroundColor = config[element.parentNode.id]["background-color"];
             element.style.fontWeight = "900";
             element.firstChild.nextSibling.style.transform = 'rotate(0deg)';
-
-            table_container = element.querySelector(".table-container");
+            
+            table_container = element.parentNode.querySelector(".table-container");
             table_container.style.display = "block"
-
-
+            
+            
         } else {
-
-            element.style.backgroundColor =
+            
+            element.style.borderBottom  = "none";
+            element.parentNode.style.backgroundColor =
                 config["general"]["div_sectionDefault"];
             element.style.fontWeight = 'normal';
 
             element.firstChild.nextSibling.style.transform = "rotate(180deg)";
-            table_container = element.querySelector(".table-container");
+            table_container = element.parentNode.querySelector(".table-container");
             table_container.style.display = "none"
         }
     });
@@ -166,14 +191,23 @@ input_div = document.createElement("div");
 
 input_checkbox = document.createElement("input");
 input_checkbox.type = "checkbox";
-input_checkbox.name = 'checkbox';
-input_checkbox.id = 'checkbox';
+input_checkbox.name = 'input-checkbox';
+input_checkbox.id = 'input-checkbox';
 input_checkbox.classList.add('input-check')
 
 input_checkbox_label = document.createElement('label')
-input_checkbox_label.setAttribute('for', "checkbox")
+input_checkbox_label.setAttribute('for', input_checkbox.id);
 input_checkbox_label.textContent = "Désactivation de hide_section";
 
 input_div.appendChild(input_checkbox)
 input_div.appendChild(input_checkbox_label)
 body.insertBefore(input_div, div_selection_table.nextSibling);
+
+// Cacher les informations des clients et les afficher au clic
+
+table_header = document.querySelectorAll(".table-header");
+Array.from(table_header).forEach(element => {
+    element.addEventListener("click", function(event) {
+        console.log(element.nextSibling)
+    })
+});
