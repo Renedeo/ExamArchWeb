9cc851e9e6c33d1f11f9d0620d33ced2d3dab2f0
Renedeo <62702704+Renedeo@users.noreply.github.com>
2023-11-15 08:14:09 +0100

    Fix : table manquante
    Pour la gestion de cette erreurs nous allons partir de la création sql à l'affichage dans la base

--- BD_Creation/BD.sql
+++ BD_Creation/BD.sql
@@ -1,64 +1,78 @@
-drop database if exists exambdbanquegestion;
-create database exambdbanquegestion;
+DROP DATABASE IF EXISTS exambdbanquegestion;
+CREATE DATABASE exambdbanquegestion;
 
--- Effacer les tables si besoins
--- Sinon utiliser alter table pour effectuer des modification 
- drop table if exists transactions;
- drop table if exists compte_client;
- drop table if exists banque;
- drop table if exists client;
-
+-- Effacer les tables si besoin
+-- Sinon utiliser alter table pour effectuer des modifications
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS compte_client;
+DROP TABLE IF EXISTS banque;
+DROP TABLE IF EXISTS client;
 
 -- Création de la table banque
-create table if not exists banque(
-	id_banque serial primary key,
-	nom varchar(50) not null,
-	localisation varchar(255) not null
-	);
+CREATE TABLE IF NOT EXISTS banque (
+	id_banque SERIAL PRIMARY KEY,
+	nom VARCHAR(50) NOT NULL,
+	localisation VARCHAR(255) NOT NULL
+);
 
 -- Création de la table client
-create table if not exists client(
-	id_client serial primary key,
-	nom varchar(50) not null,
-	prenom varchar(50) not null,
-	-- importante car permet de pouvoir créer un compte
-	date_de_naissance date not null,
-	situation_maritale varchar(50) not null
-	);
+CREATE TABLE IF NOT EXISTS client (
+	id_client SERIAL PRIMARY KEY,
+	nom VARCHAR(50) NOT NULL,
+	prenom VARCHAR(50) NOT NULL,
+	-- Importante car permet de pouvoir créer un compte
+	date_de_naissance DATE NOT NULL,
+	situation_maritale VARCHAR(50) NOT NULL
+);
+
+-- Création de la table des conseillers
+CREATE TABLE IF NOT EXISTS conseiller (
+	id_conseiller SERIAL PRIMARY KEY,
+	id_banque INT,
+	CONSTRAINT fk_banque_conseiller
+		FOREIGN KEY (id_banque)
+		REFERENCES banque(id_banque),
+	nom VARCHAR(50) NOT NULL,
+	prenom VARCHAR(50) NOT NULL
+);
 
 -- Création de la table Compte client
-create table if not exists compte_client(
-	id_compte serial primary key, 
-	id_client int,
-	id_banque int,
-	constraint fk_banque_compte
-		foreign key (id_banque)
-			references banque(id_banque),
-	constraint fk_client_compte
-		foreign key (id_client)
-			references client(id_client),
-	-- Par défaut le solde d'un compte est à 0
-		solde real default 0,
-	-- On ne peut omettre la date de creation 
-	-- Mais la date de resiliation peut être à nulle 
+CREATE TABLE IF NOT EXISTS compte_client (
+	id_compte SERIAL PRIMARY KEY, 
+	id_client INT,
+	id_banque INT,
+	id_conseiller INT,
+	CONSTRAINT fk_banque_compte
+		FOREIGN KEY (id_banque)
+		REFERENCES banque(id_banque),
+	CONSTRAINT fk_client_compte
+		FOREIGN KEY (id_client)
+		REFERENCES client(id_client),
+	CONSTRAINT fk_conseiller_compte
+		FOREIGN KEY (id_conseiller)
+		REFERENCES conseiller(id_conseiller),
+	-- Par défaut, le solde d'un compte est à 0
+	solde REAL DEFAULT 0,
+	-- On ne peut omettre la date de création 
+	-- Mais la date de résiliation peut être nulle 
 	-- tant que le client n'a pas résilié son compte
-		date_creation date not null default now(),
-		date_resiliation date,
-		type_de_compte varchar(255)
-	);
+	date_creation DATE NOT NULL DEFAULT NOW(),
+	date_resiliation DATE,
+	type_de_compte VARCHAR(255)
+);
 
 -- Création de la table Transactions 
-create table if not exists transactions(
-	id_transactions serial primary key,
-	id_compte_exp int,
-	id_compte_dest int,
-	constraint fk_compte_exp_transactions
-		foreign key (id_compte_exp)
-			references compte_client(id_compte),
-	constraint fk_compte_dest_transactions
-		foreign key (id_compte_dest)
-			references compte_client(id_compte),
-	montant real not null,
-	motif varchar(255) not null,
-	date_de_transaction date not null default now()
+CREATE TABLE IF NOT EXISTS transactions (
+	id_transactions SERIAL PRIMARY KEY,
+	id_compte_exp INT,
+	id_compte_dest INT,
+	CONSTRAINT fk_compte_exp_transactions
+		FOREIGN KEY (id_compte_exp)
+		REFERENCES compte_client(id_compte),
+	CONSTRAINT fk_compte_dest_transactions
+		FOREIGN KEY (id_compte_dest)
+		REFERENCES compte_client(id_compte),
+	montant REAL NOT NULL,
+	motif VARCHAR(255) NOT NULL,
+	date_de_transaction DATE NOT NULL DEFAULT NOW()
 );
